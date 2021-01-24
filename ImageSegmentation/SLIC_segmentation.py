05d4f27e8cf1f8d07d7c30ac1a8b3ae63a2c4b29
Jagan <jaganmohan.chandrasekaran@mavs.uta.edu>
2021-01-23 19:59:32 -0600

    Weekly push

--- ImageSegmentation/SLIC_segmentation.py
+++ ImageSegmentation/SLIC_segmentation.py
@@ -17,8 +17,19 @@ def generate_segments(imageLocation):
     ax = fig.add_subplot(1, 1, 1)
     ax.imshow(mark_boundaries(img_as_float(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)), segments))
     plt.axis("off")
+    #new logic
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+            hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig("/Users/Jagan/Desktop/5052segmentation1.jpg", bbox_inches = 'tight',
+    pad_inches = 0)
+    # new logic e
+    plt.savefig('/Users/Jagan/Desktop/5052segmentation2.jpg', dpi=300) # add bbox_inches to remove the white space padding
+    #plt.imsave('/Users/Jagan/Desktop/2304segmentation.jpg', imageLocation) # add bbox_inches to remove the white space padding
     plt.show()
-
     print(np.unique(segments))
 
     # To mask
@@ -44,12 +55,12 @@ def generate_segments(imageLocation):
     #     cv2.imshow("Mask", mask)
     #     cv2.imshow("Applied", cv2.bitwise_and(image, image, mask=mask))
     #     cv2.waitKey(0)
-
+    '''
     mask = np.zeros(image.shape[:2], dtype='uint8')
-    #masked_segments = [0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,21]
-    masked_segments = [1,4,5,6,7,8,9,10,11,12,13,17,18,21]
+    masked_segments = [2,4]
+    #masked_segments = [1,4,5,6,7,8,9,10,11,12,13,17,18,21]
     #masked_segments = [9,14]
-    output_file_destination = '/Users/Jagan/Desktop/Image4837_failure_inducing_combination_explainer_new'+str(masked_segments)+'.jpg'
+    output_file_destination = '/Users/Jagan/Desktop/Image833_failure_inducing_combination_explainer'+str(masked_segments)+'.jpg'
     for i in np.unique(segments):
         print i
         if i not in masked_segments:
@@ -58,7 +69,7 @@ def generate_segments(imageLocation):
             cv2.imshow('Modified_Image', cv2.bitwise_and(image, image, mask=mask))
             cv2.waitKey(0)
     cv2.imwrite(output_file_destination, cv2.bitwise_and(image, image, mask=mask))
-
+    '''
 
 
 if __name__ == '__main__':
--- Misc/imageViewer.py
+++ Misc/imageViewer.py
@@ -11,24 +11,37 @@ def displayImage(imageDirectory):
     targetLocation = imageDirectory + '/*.jpg'
     print targetLocation
     for img_path in glob.glob(targetLocation):
+        print img_path
         images.append(mpimg.imread(img_path))
 
-
-    plt.figure(figsize=(20, 10))
-    columns = 5
+    
+    plt.figure(figsize=(20, 20))
+    #columns = 5
+    columns = 3
     for i, image in enumerate(images):
+        #print image
         plt.subplot(len(images) / columns + 1, columns, i + 1)
         plt.imshow(image)
-        plt.title('Test case: '+ str(i))
+        #plt.title('Test case: '+ str(i))
     #plt.show()
-    plt.savefig('/Users/Jagan/Desktop/image3104_initial_test_cases.jpg', dpi=300)
-
+    #plt.savefig('/Users/Jagan/Desktop/explanations2.jpg', dpi=300)
+    plt.savefig('/Users/Jagan/Desktop/explanations2.jpg', dpi=300)
+    # new logic b
+    # plt.gca().set_axis_off()
+    # plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+    #         hspace = 0, wspace = 0)
+    # plt.margins(0,0)
+    # plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    # plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    # plt.savefig("/Users/Jagan/Desktop/explanations2.jpg", bbox_inches = 'tight',
+    # pad_inches = 0)
+    # new logic e
 
 
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image_directory', type=str)
-    
+
     args, unknown = parser.parse_known_args()
     displayImage(args.image_directory)
